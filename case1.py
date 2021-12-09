789710cdf6541465102736a3b1ed59d664b3ff75
Sergio Tabares <alu0101124896@ull.edu.es>
2021-12-09 11:28:40 +0000

    Case1 basic structure added

---
+++ case1.py
@@ -0,0 +1,11 @@
+def case1(clause, i):
+
+
+    return variables, clauses
+
+
+if __name__ == "__main__":
+    variables, clauses = case1(data['C'][0], 3)
+
+    print(variables)
+    print(clauses)
